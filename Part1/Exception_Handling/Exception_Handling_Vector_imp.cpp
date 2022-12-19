9a4d595e0f081064115f85de11c1aea21d0c30b7
Demonically Inspired <lustyambrose123@gmail.com>
2022-12-19 14:57:27 +0530

    Done with representing memory explicitly

--- Part1/Exception_Handling/Exception_Handling_Vector_imp.cpp
+++ Part1/Exception_Handling/Exception_Handling_Vector_imp.cpp
@@ -16,35 +16,59 @@ void unintialized_copy1(T* beg, T* end, const T& val){
   }
 }
 template<class T, class A = allocator<T>>
+struct Vector_base{
+  A alloc; 
+  T* elem; 
+  T* space; 
+  T* last; 
+  Vector_base(const A& a, size_t n): alloc{a}, elem{alloc.allocate(n)}, space{elem + n}, last{elem + n}{}; 
+  ~Vector_base(){alloc.deallocate(elem, last - elem);}
+  Vector_base(const Vector_base& v1) = delete; //since vector_base is explicitly designed to represent memeory for Vector class, which can be used anywhere copy operations are deleted operation for such calss. 
+  Vector_base& operator = (const Vector_base& v1) = delete; 
+  Vector_base(Vector_base&& v1): alloc{v1.alloc}, 
+                                 elem{v1.elem},
+                                 last{v1.last}, 
+                                 space{v1.space}{
+    v1.elem = v1.space = v1.last = v1.alloc = nullptr; 
+  }//Properly transffer ownership after the move operations. 
+  Vector_base& operator=(Vector_base&& v1){
+    swap(*this, v1); 
+    return *this; 
+  } 
+};
+
+template<class T, class A = allocator<T>>
 class Vector{
 private: 
-  T* elem; 
-  T* last; 
-  T* space; 
-  A alloc; 
-public: 
-  Vector(size_t n, const T& val = T(), A a = A()): alloc{a}{
-    elem = alloc.allocate(n); 
-    last = space = (elem + n); 
-    try{
-      unintialized_copy1(elem, last, val); 
-  }catch(...){
-      alloc.deallocate(elem, n); 
-      throw; 
+  Vector_base<T, A>vb; 
+  void destroy_elems(){
+  for(T* p = vb.elem; p != vb.last; p++){
+      p->~T(); 
     }
+    vb.space = vb.elem; 
+  }; 
+public: 
+  Vector(size_t n, const T& val = T(), A a = A()): vb{a, n}{
+    unintialized_copy1(vb.elem, vb.elem + n, val); 
   }
   Vector(const Vector& v1); 
-  Vector(Vector&& v1); 
-  size_t size(){return space  - elem;}
-  size_t capacity(){return last - elem;}
+  Vector(Vector&& v1): vb{std::move(v1.vb)}{};   
+  Vector& operator=(Vector& v1){
+    swap(*this, v1); 
+    return *this; 
+  }
+  size_t size(){return vb.space  - vb.elem;}
+  size_t capacity(){return vb.last - vb.elem;}
   void resize(size_t n, const T&  = T{}); 
   void reserve(size_t n); 
   void push_back(const T& value); 
   T& operator [](const int index){
     if(index < 0 || index > size()){throw std::out_of_range{"out of range"};}
-    return elem[index]; 
+    return vb.elem[index]; 
   }
-  ~Vector(){}
+  ~Vector(){
+    destroy_elems(); 
+  }
 };
 class Points{
   private: 
